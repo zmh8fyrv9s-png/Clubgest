bb9e61ea744c41da5d08417b9120a5258c03ead9
zmh8fyrv9s-png <zmh8fyrv9s@privaterelay.appleid.com>
2026-09-02 22:53:01 +0200

    feat: add ClubGest offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='clubgest-v1';
+const ASSETS=['/','/index.html','/manifest.webmanifest','/icon-192.svg','/icon-512.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response}).catch(()=>caches.match('/index.html'))));});
